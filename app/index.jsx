9ed212a1e4772ccedec12a70eedff46512d76bdb
AliAch04 <ali.jallousy12@gmail.com>
2025-03-04 21:30:47 +0000

    adding some styling to the home page

---
+++ app/index.jsx
@@ -0,0 +1,45 @@
+import { Text, View, StyleSheet, Image } from "react-native";
+import PostItImage from "@/assets/images/post-it.png"
+
+const HomeScreen = () => {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={PostItImage} style={styles.image}/>
+      <Text style={styles.title}>Welcome to notes App</Text>
+      <Text style={styles.subtitle}>Capture your thoughs anytime, anywhere</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: '#f8f9fa'
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333'
+
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#666'
+  }
+});
+
+export default HomeScreen;
