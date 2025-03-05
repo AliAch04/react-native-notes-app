080f8ba5ee15aa4fc9c6f93ada73dd55e883ad73
AliAch04 <ali.jallousy12@gmail.com>
2025-03-05 11:37:56 +0000

    cleaning code _add components folder

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Modal, TextInput } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity,} from "react-native";
+import NoteList from "@/components/NoteList";
+import AddNoteModal from "@/components/AddNoteModal";
 
 const NoteScreen = () =>{
     const [notes, setNotes] = useState([
@@ -25,55 +27,27 @@ const NoteScreen = () =>{
 
     return (
         <View style = {styles.container}>
-            <FlatList 
-                data={notes}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <View style={styles.noteItem}>
-                        <Text style={styles.noteText}>{item.text}</Text>
-                    </View>
-                )}
-            />
+            
+            <NoteList notes={notes} />
 
             <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
                 <Text style={styles.addButtonText}>+ Add Note</Text>
             </TouchableOpacity>
 
-            <Modal
-                visible={modalVisible}
-                animationType="slide"
-                transparent
-                onRequestClose={() => setModalVisible(false)}
-            >
-                <View style={styles.modalOverLay}>
-                    <View style={styles.modalContent}>
-                        <Text style={styles.modalTitle}>Add a New Note</Text>
-                        <TextInput 
-                            style={styles.input}
-                            placeholder="Enter your note..."
-                            placeholderTextColor='#aaa'
-                            value={newNote}
-                            onChangeText={setNewNote}
-                        />
-                        <View style={styles.modalButtons}>
-                            <TouchableOpacity style={styles.cancelButton} onPress={ () => setModalVisible(false)}>
-                                <Text style={styles.cancelButtonText}>Cancle</Text>
-                            </TouchableOpacity>
+            <AddNoteModal 
+                modalVisible={modalVisible}
+                setModalVisible={setModalVisible}
+                newNote={newNote}
+                setNewNote={setNewNote}
+                addNote={addNote}
+            />
 
-                            <TouchableOpacity style={styles.saveButton} onPress={addNote}>
-                                <Text style={styles.saveButtonText}>Save</Text>
-                            </TouchableOpacity>
-
-                        </View>
-                    </View>
-                </View>
-            </Modal>
         </View>
     );
 };
 
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
@@ -107,60 +81,7 @@ const styles = StyleSheet.create({
         color: '#fff',
         
     },
-    modalOverLay: {
-        flex: 1,
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalContent: {
-        backgroundColor: '#fff',
-        padding: 20,
-        borderRadius: 10,
-        width: '80%',
-    },
-    modalTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-        textAlign: 'center',
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 8,
-        padding: 10,
-        fontSize: 16,
-        marginBottom: 15,
-    },
-    modalButtons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    cancelButton: {
-        backgroundColor: '#ccc',
-        padding: 10,
-        borderRadius: 5,
-        flex: 1,
-        marginRight: 10,
-        alignItems: 'center'
-    },
-    cancelButtonText: {
-        fontSize: 16,
-        color: '#333'
-    },
-    saveButton: {
-        backgroundColor: '#007bff',
-        padding: 10,
-        borderRadius: 5,
-        flex: 1,
-        marginleft: 10,
-        alignItems: 'center'
-    },
-    saveButtonText: {
-        fontSize: 16,
-        color: '#fff'
-    },
+    
 
 })
 
---
+++ components/NoteItems.jsx
@@ -0,0 +1,10 @@
+import { styles } from "@/app/notes";
+import { View, Text, } from "react-native";
+
+const NoteItem = ({ note }) =>{
+    return <View style={styles.noteItem}>
+                <Text style={styles.noteText}>{note.text}</Text>
+            </View>
+}
+
+export default NoteItem ;
